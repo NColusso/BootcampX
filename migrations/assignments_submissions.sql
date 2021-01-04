778547930801ead3516c65349dfcde41ae0ce2da
Natasha Colusso <natashacolusso@gmail.com>
2021-01-04 23:55:04 +0000

    BUG FIX corrected in table assignments day to have value of integer (previously was date)

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -2,7 +2,7 @@ CREATE TABLE assignments (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   content TEXT,
-  day DATE,
+  day INTEGER,
   chapter INTEGER,
   duration INTEGER
 );
@@ -13,4 +13,4 @@ CREATE TABLE assignment_submissions (
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
-);
+); 

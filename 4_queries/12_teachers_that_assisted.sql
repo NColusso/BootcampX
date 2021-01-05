bfe87ca41e8bb3d9520f1b0a8bbbecedfa0e8561
Natasha Colusso <natashacolusso@gmail.com>
2021-01-05 21:13:48 +0000

    SQL QUERIES using teachers assistance/teachers tables

---
+++ 4_queries/12_teachers_that_assisted.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN teachers ON teachers.id = teacher_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+-- GROUP BY cohorts.name, teachers.name (distinct at top instead)
+ORDER BY teachers.name;

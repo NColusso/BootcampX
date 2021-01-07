8a7dd700494d43dc6816141dbaba77bf3fd114ce
Natasha Colusso <natashacolusso@gmail.com>
2021-01-07 03:49:06 +0000

    SECURITY UPDATE: updated to use parameterized query for increased security

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,19 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const queryString = `
+SELECT students.id AS student_id, students.name, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
 pool
-  .query(
-    `
-  SELECT students.id AS student_id, students.name, cohorts.name AS cohort
-  FROM students
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-`
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,19 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
+const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN teachers ON teachers.id = teacher_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = $1
+ORDER BY teachers.name;`;
+
+const cohort = process.argv[2] || "JUL02";
+const values = [cohort];
+
 pool
-  .query(
-    `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-  FROM assistance_requests
-  JOIN students ON students.id = student_id
-  JOIN teachers ON teachers.id = teacher_id
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${process.argv[2]}'
-  ORDER BY teachers.name;`
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((assistance) => {
       console.log(`${assistance.cohort}: ${assistance.teacher}`);
